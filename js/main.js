56f190c8af7ff60b276580a660034932a5968ff4
Marcin <xcinek1337@wp.pl>
2023-03-08 16:50:56 +0100

    website finished

--- js/main.js
+++ js/main.js
@@ -1,16 +1,29 @@
-const navMobile = document.querySelector('.nav-mobile');
-const navBtn = document.querySelector('.hamburger');
-const footerYear = document.querySelector('.footer__year');
+const navMobile = document.querySelector('.nav-mobile')
+const navBtn = document.querySelector('.hamburger')
+const footerYear = document.querySelector('.footer__year')
+const navLinks = document.querySelectorAll('.nav__link')
+const body = document.querySelector('body')
 
 const handleNav = () => {
-    navBtn.classList.toggle('is-active')   
-    navMobile.classList.toggle('nav-mobile--active')   
+	navBtn.classList.toggle('is-active')
+	navMobile.classList.toggle('nav-mobile--active')
+
+	navLinks.forEach(link => {
+		link.addEventListener('click', () => {
+			navBtn.classList.remove('is-active')
+			navMobile.classList.remove('nav-mobile--active')
+		})
+	})
 }
 
-navBtn.addEventListener('click', handleNav);
+navBtn.addEventListener('click', handleNav,)
+
+
 
 const handleCurrentYear = () => {
 	const year = new Date().getFullYear()
 	footerYear.innerText = year
 }
-handleCurrentYear();
+handleCurrentYear()
+
+
